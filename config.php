6b48736abad269b4aef934146af17feb3f4f47e6
antiproton <dr.urban@netreal.de>
2014-11-11 08:12:25 +0100

    configuration file

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#-------------------------------------------------------------------------
+# 15.1.2014 - Ralf Urban
+#-------------------------------------------------------------------------
+
+#Schach Bot
+$botty = false; #true
+
+#Path
+$bot_path = "C:/xampp/htdocs/chess";
+
+
+#Url
+$bot_url="http://localhost/brett1";
+
+
+# Nach der Wartezeit ist die Partie wieder frei
+$wartezeit_auf_gegner =20;
+
+# Bei Überschreitung ist die Partie verloren
+$partie_zeit = 300;
+
+# Die Partie wird nach einer bestimmten Zeit gelöscht
+$del_partie_zeit = 800;
+
+# Wenn der Gegner nicht zieht kann auf Gewinn reklamiert werden
+$reklamation_zeit = 30;
+
+?>
